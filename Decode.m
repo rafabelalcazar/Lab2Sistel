23a3a42ec1242ac7a1017134ff61139710b4b3fc
rafa <rafaelb@unicauca.edu.co>
2020-03-04 17:09:02 -0500

    FEAT: Prueba función Decode correcta

--- Decode.m
+++ Decode.m
@@ -4,7 +4,27 @@ function decodeMsg = Decode (mCoded)
     %Se define la matriz H
     H=[1 0 0 1 0 1 1;0 1 0 1 1 1 0;0 0 1 0 1 1 1];
     
+    %Se establecen los sindromes con los cuales se realizara la correción del mensaje
+    s1=[1 1 0];                             
+    s2=[0 1 1];
+    s3=[1 1 1];
+    s4=[1 0 1];
+    
     %Si no existen errores el vector Sindrome será [0 0 0]
     sindrome = rem(mCoded*H',2);
-    decodeMsg = sindrome;
+    if(sindrome == s1 )
+        decodeMsg = rem(mCoded+[0 0 0 1 0 0 0],2);
+        decodeMsg = decodeMsg(4:end);
+    elseif(sindrome == s2 )
+        decodeMsg = rem(mCoded+[0 0 0 0 1 0 0],2);
+        decodeMsg = decodeMsg(4:end);
+    elseif(sindrome == s3)
+        decodeMsg = rem(mCoded+[0 0 0 0 0 1 0],2);
+        decodeMsg = decodeMsg(4:end);
+    elseif(sindrome == s4)
+        decodeMsg = rem(mCoded+[0 0 0 0 0 0 1],2);
+        decodeMsg = decodeMsg(4:end);
+    else
+        decodeMsg=mCoded(4:end);
+    end
 end
